27db9f4b547bc6abda833eb943f89f38c0fd9825
Brett Tolbert <bretttolbert@gmail.com>
2026-09-07 18:19:10 -0500

    change arg name

--- src/mediatest/main.py
+++ src/mediatest/main.py
@@ -9,7 +9,7 @@ from mediatest.config import configure
 def main(config_path: Path | None = None) -> int:
     if config_path is None:
         parser = argparse.ArgumentParser()
-        parser.add_argument("config-path", type=Path, nargs="?")
+        parser.add_argument("config_path", type=Path, nargs="?")
         config_path = parser.parse_args().config_path
 
     configure(config_path)
